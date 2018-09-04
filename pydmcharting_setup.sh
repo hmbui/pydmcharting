b98acb2bc317b98de22c2d8b126090b19379990a
Hai Bui <hbui@slac.stanford.edu>
2018-09-03 18:04:05 -0700

    ENH: Add bash env setup script.

--- pydmcharting_setup.sh
+++ pydmcharting_setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-echo Setting up environment variables...
-export PYDM_STRIPTOOL_PYDM_PATH=$PACKAGE_TOP/pydm/setup_pydm_env.bash
+echo Setting up environment variables for PyDM Charting...
+
+source /afs/slac/g/lcls/package/pydm/setup_pydm_env.bash
+export PYDM_PATH=/u/re/hbui/local/dev_test/pydm/pydm-hmbui
+export PYDM_STRIPTOOL_PYDM_PATH=/u/re/hbui/local/dev_test/pydm/pydm-hmbui
+export PYTHONPATH=$PYDM_PATH:/u/re/hbui/local/dev_test/pydmcharting/pydmcharting-hmbui:$PYTHONPATH
+export EPICS_CA_AUTO_ADDR_LIST=yes
+
 echo Done!
