7d47b6eec14b0c65adf5df57d392e9f4b1948d9f
Hai Bui <hbui@slac.stanford.edu>
2018-09-06 14:56:26 -0700

    BUG: Fix the exported path to the libca.so

--- pydmcharting_setup.sh
+++ pydmcharting_setup.sh
@@ -7,6 +7,6 @@ source /afs/slac/g/lcls/package/pydm/setup_pydm_env.bash
 export PYDM_PATH=/u/re/hbui/local/dev_test/pydm/pydm-hmbui
 
 export EPICS_CA_AUTO_ADDR_LIST=yes
-export PYEPICS_LIBCA=/afs/slac/g/lcls/epics/base/R3.15.5-1.0/lib/rhel6-x86_64/libca.s
+export PYEPICS_LIBCA=/afs/slac/g/lcls/epics/base/R3.15.5-1.0/lib/rhel6-x86_64/libca.so
 
 echo Done!
