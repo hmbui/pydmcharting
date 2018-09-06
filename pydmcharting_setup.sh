62b312d2ee8036389fec0ee5c5caeadb96cb1972
Hai Bui <hbui@slac.stanford.edu>
2018-09-06 14:51:23 -0700

    BUG: Add missing __init__.py for the exporters module. BUG: Fix milliseconds to days converting bug. ENH: Reduce the number of env variables, and export one for the libca library.

--- pydmcharting_setup.sh
+++ pydmcharting_setup.sh
@@ -3,9 +3,10 @@
 echo Setting up environment variables for PyDM Charting...
 
 source /afs/slac/g/lcls/package/pydm/setup_pydm_env.bash
+
 export PYDM_PATH=/u/re/hbui/local/dev_test/pydm/pydm-hmbui
-export PYDM_STRIPTOOL_PYDM_PATH=/u/re/hbui/local/dev_test/pydm/pydm-hmbui
-#export PYTHONPATH=$PYDM_PATH:/u/re/hbui/local/dev_test/pydmcharting/pydmcharting-hmbui:$PYTHONPATH
+
 export EPICS_CA_AUTO_ADDR_LIST=yes
+export PYEPICS_LIBCA=/afs/slac/g/lcls/epics/base/R3.15.5-1.0/lib/rhel6-x86_64/libca.s
 
 echo Done!
